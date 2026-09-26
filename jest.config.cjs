224968ca71fad2d9e010b0f81ab6a5ac6069bab4
DaoNgocAnh25092004 <161846189+DaoNgocAnh25092004@users.noreply.github.com>
2026-09-26 14:15:04 +0700

    build(config): configure shipping path aliases

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,14 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: '.',
-  testRegex: '.*\\.spec\\.ts$',
-  transform: { '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: 'tsconfig.spec.json' }] },
-  collectCoverageFrom: ['src/**/*.ts'],
-  coverageDirectory: './coverage',
-  testEnvironment: 'node',
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: '.',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+        '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: 'tsconfig.spec.json' }],
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    collectCoverageFrom: ['src/**/*.ts'],
+    coverageDirectory: './coverage',
+    testEnvironment: 'node',
 };
